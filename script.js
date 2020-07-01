c848d343a1fb48a89d6a3a88e44598983882cf4e
TaimoorSalyhal <asfandtaimoor786@gmail.com>
2020-07-01 17:26:26 +0500

    Basic game setup

---
+++ script.js
@@ -0,0 +1,40 @@
+// Player  status
+
+
+let currentPlayer = true;
+let player_1 = false; // times
+let Player_2 = false; //circle
+
+
+player_1 = currentPlayer; // active player
+
+// click the block to show the times or circle according to the player term
+
+function show(elem) {
+    let block = document.querySelector(`.${elem}`);
+    let times = block.querySelector('.fa-times'); //player 1 symbol
+    let circle = block.querySelector('.fa-circle'); // player 2 symbol
+
+
+
+    // for payer 1 (Times Player ) to show circle when clicked
+    if (player_1 === true) {
+        if (times.classList.contains("hide") == true) {
+            times.classList.add("show");
+        }
+
+
+        player_1 = false;
+        Player_2 = currentPlayer;
+    } else {
+        if (circle.classList.contains("hide") == true) {
+            circle.classList.add("show");
+        }
+
+
+        Player_2 = false;
+        player_1 = currentPlayer;
+    }
+
+
+}
